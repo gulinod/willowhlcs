1f9a2ac2322b96290d5bf7ad7bf19aa64d11c0e6
gulinod <gulinodan@gmail.com>
2016-08-30 20:40:00 -0400

    Second initial commit

--- www/js/routes.js
+++ www/js/routes.js
@@ -2,7 +2,7 @@ angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
+  // Ionic uses AngularUI Router which uses the concept of statesg
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
