e74633b78d963e4cf24154daa56fd73705cc5d9e
gulinod <gulinodan@gmail.com>
2016-08-30 20:30:15 -0400

    Initial commit

---
+++ www/js/routes.js
@@ -0,0 +1,36 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+
+      .state('cartTabDefaultPage', {
+    url: '/page3',
+    templateUrl: 'templates/cartTabDefaultPage.html',
+    controller: 'cartTabDefaultPageCtrl'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+    .state('rgbColorPicker', {
+      url: '/page2',
+      templateUrl: 'templates/colorPicker.html',
+      controller : "rgbController"
+
+    }
+  )
+
+$urlRouterProvider.otherwise('/page3')
+
+
+
+});
